e28fd9ca9118054137aaa8aa5f8a51d900f7e1e4
Karabo Matabane <2017285772@ufs4life.ac.za>
2022-04-12 11:56:46 +0200

    End of section 12

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -6,6 +6,7 @@ import {TabsModule} from 'ngx-bootstrap/tabs'
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker'
 
 
 
@@ -20,7 +21,8 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     TabsModule.forRoot(),
     NgxGalleryModule,
     FileUploadModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    BsDatepickerModule.forRoot()
     
   ],
   exports: [
@@ -29,7 +31,8 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     TabsModule,
     NgxGalleryModule,
     FileUploadModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    BsDatepickerModule
   ]
 })
 export class SharedModule { }
